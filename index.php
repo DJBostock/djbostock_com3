385d4a806d489d7d4d46f585f73557a13cbf11b4
DJ Bostock <douglasjbostock@gmail.com>
2020-05-29 12:00:00 -0500

    cumulative update

--- index.php
+++ index.php
@@ -14,5 +14,6 @@ $title = "Home";
         <h2>A Secondary Header</h2>
         <p>This is some main content.</p>
         <h2>Another Secondary Header</h2>
+        <p>This is some more main content.</p>
     </body>
 </html>
