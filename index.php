f3df66048ebda24ba2543cca447591db17c177be
DJ Bostock <douglasjbostock@gmail.com>
2020-05-30 12:00:00 -0500

    cumulative update

--- index.php
+++ index.php
@@ -6,14 +6,17 @@ $title = "Home";
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <title><?= $title; ?></title>
-    </head>
-    <body>
-        <h1>The Primary Header</h1>
-        <h2>A Secondary Header</h2>
-        <p>This is some main content.</p>
-        <h2>Another Secondary Header</h2>
-        <p>This is some more main content.</p>
-    </body>
+
+<head>
+    <title><?= $title; ?></title>
+</head>
+
+<body>
+    <h1>The Primary Header</h1>
+    <h2>A Secondary Header</h2>
+    <p>This is some main content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum magni ratione mollitia velit, neque eum molestias, tempora nam cupiditate veritatis eius accusamus. Ex, rerum. Dolorem rem veniam modi animi sit?</p>
+    <h2>Another Secondary Header</h2>
+    <p>This is some more main content. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa amet cum obcaecati quas sint impedit ex eaque itaque, accusantium blanditiis possimus animi tempora, expedita est voluptas assumenda explicabo ad libero.</p>
+</body>
+
 </html>
