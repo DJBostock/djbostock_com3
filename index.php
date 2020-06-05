2f9e7a3d56a73fc1af5d0b963d5402c8b29fe32d
DJ Bostock <douglasjbostock@gmail.com>
2020-06-05 12:00:00 -0500

    cumulative update

--- index.php
+++ index.php
@@ -13,12 +13,14 @@ $title = "Home";
 </head>
 
 <body>
-    <h1>The Primary Header</h1>
-    <img src="/images/happy-stick.jpg" class="hero">
-    <h2>A Secondary Header</h2>
-    <p>This is some main content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum magni ratione mollitia velit, neque eum molestias, tempora nam cupiditate veritatis eius accusamus. Ex, rerum. Dolorem rem veniam modi animi sit?</p>
-    <h2>Another Secondary Header</h2>
-    <p>This is some more main content. <a href="https://wikipedia.org">Wikipedia</a> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa amet cum obcaecati quas sint impedit ex eaque itaque, accusantium blanditiis possimus animi tempora, expedita est voluptas assumenda explicabo ad libero.</p>
+    <div class="container">
+        <h1>The Primary Header</h1>
+        <img src="/images/happy-stick.jpg" class="hero">
+        <h2>A Secondary Header</h2>
+        <p>This is some main content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum magni ratione mollitia velit, neque eum molestias, tempora nam cupiditate veritatis eius accusamus. Ex, rerum. Dolorem rem veniam modi animi sit?</p>
+        <h2>Another Secondary Header</h2>
+        <p>This is some more main content. <a href="https://wikipedia.org">Wikipedia</a> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa amet cum obcaecati quas sint impedit ex eaque itaque, accusantium blanditiis possimus animi tempora, expedita est voluptas assumenda explicabo ad libero.</p>
+    </div>
 </body>
 
 </html>
