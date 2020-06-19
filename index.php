6fa6508bd92c2dca77ccfdfdca3e3b9b4cafe4a0
DJ Bostock <douglasjbostock@gmail.com>
2020-06-19 12:00:00 -0500

    cumulative update

--- index.php
+++ index.php
@@ -24,9 +24,11 @@ $title = "Home";
                 <h2>A Secondary Header</h2>
                 <p>This is some main content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum magni ratione mollitia velit, neque eum molestias, tempora nam cupiditate veritatis eius accusamus. Ex, rerum. Dolorem rem veniam modi animi sit?</p>
             </section>
+            <section>
+                <h2>Another Secondary Header</h2>
+                <p>This is some more main content. <a href="https://wikipedia.org">Wikipedia</a> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa amet cum obcaecati quas sint impedit ex eaque itaque, accusantium blanditiis possimus animi tempora, expedita est voluptas assumenda explicabo ad libero.</p>
+            </section>
         </main>
-        <h2>Another Secondary Header</h2>
-        <p>This is some more main content. <a href="https://wikipedia.org">Wikipedia</a> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa amet cum obcaecati quas sint impedit ex eaque itaque, accusantium blanditiis possimus animi tempora, expedita est voluptas assumenda explicabo ad libero.</p>
     </div>
 </body>
 
