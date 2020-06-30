39768f95e41092e8a3bfd2ff9b3bf0d30ddfdc5a
DJ Bostock <douglasjbostock@gmail.com>
2020-06-30 12:00:00 -0500

    cumulative update

--- index.php
+++ index.php
@@ -29,14 +29,18 @@ $title = "DJ Bostock";
             <section id="home">
                 <h2>Welcome!</h2>
                 <p>Welcome to my page. It exists primarily as a sandbox for me to try stuff out.</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque velit magni voluptate officia placeat nam suscipit dolores quaerat, non omnis dignissimos voluptas alias quae dolorum optio cupiditate ut aut quia?</p>
             </section>
             <section id="aboutMe">
                 <h2>About Me</h2>
                 <p>All the things you ever wanted to know...</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim laudantium voluptatem tenetur expedita, blanditiis quas veritatis dicta fugit veniam id? Accusamus, quas deleniti! Quia itaque ullam quae maxime culpa quas.</p>
             </section>
             <section id="projects">
                 <h2>Projects</h2>
                 <p>Some very impressive representations of work done...</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga, accusamus velit. Enim officia dolore consequatur beatae! Facilis, odio repellat est dolorum explicabo eaque nihil ducimus harum sed sint fugiat aperiam?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel eaque amet fugit corrupti aut, numquam fugiat quae, at magni, earum harum sit dignissimos deleniti necessitatibus. Magni modi ad praesentium non!</p>
             </section>
             <aside></aside>
         </main>
