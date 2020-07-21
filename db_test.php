ee0b4721e4b3216c3da87242e3fceaf5e9d8f8e2
DJ Bostock <douglasjbostock@gmail.com>
2020-07-21 12:00:00 -0500

    cumulative update

--- db_test.php
+++ db_test.php
@@ -8,5 +8,50 @@ if (mysqli_connect_error()) {
     echo mysqli_connect_error();
     exit;
 } else {
-    echo "connected successfully";
+    $sql = "SELECT * FROM article ORDER BY created_at;";
+
+    $results = mysqli_query($conn, $sql);
+
+    if ($results === false) {
+        echo mysqli_error($conn);
+    } else {
+        $articles = mysqli_fetch_all($results, MYSQLI_ASSOC);
+        // var_dump($articles);
+    }
 }
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <h1>My Blog</h1>
+    </header>
+    <main>
+        <?php if (empty($articles)): ?>
+            <p>No articles found.</p>
+        <?php else: ?>
+            <ul>
+                <?php foreach ($articles as $article): ?>
+                    <li>
+                        <section>
+                            <h2><?= $article['title']; ?></h2>
+                            <p><?= $article['content']; ?></p>
+                        </section>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+    </main>
+
+</body>
+
+</html>
