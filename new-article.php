cb54249ed823e7e740be5b8b08b7c84e5fc4490c
DJ Bostock <douglasjbostock@gmail.com>
2020-08-13 12:00:00 -0500

    cumulative update

---
+++ new-article.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New Article</title>
+</head>
+
+<body>
+    <header>
+        <h1>New Article</h1>
+    </header>
+    <main>
+        <form action="new-article.php" method="post">
+            <div>
+                <label for="article_title">Title:</label>
+                <input type="text" name="article_title" id="article_title" autofocus>
+            </div>
+            <div>
+                <label for="article_body">Message:</label>
+                <textarea name="article_body" id="article_body"></textarea>
+            </div>
+            <button>Send</button>
+        </form>
+    </main>
+</body>
+
+</html>
